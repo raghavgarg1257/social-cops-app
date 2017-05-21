7d9825f5502904099ac3175f4e1875574b9ba700
Raghav Garg <ansraghav100@gmail.com>
2017-05-21 21:10:40 +0530

    image thumbnail done

--- app/controllers/imgThumb.js
+++ app/controllers/imgThumb.js
@@ -1,5 +1,11 @@
 "use strict";
 
+import im from 'imagemagick';
+import uuidV4 from 'uuid/v4';
+import http from 'http';
+import fs from 'fs';
+import fileType from 'file-type';
+
 import HTTP from "../helpers/httpcodes";
 import { isExist } from "../helpers/methods";
 
@@ -12,6 +18,68 @@ module.exports = (router, middlewares) => {
         .post( (req, res, next) => {
 
             const url = req.body.url;
+
+            http.get(url, imgRes => {
+
+                let ext = '', imagedata = '';
+
+                imgRes.once('data', chunk => {
+                    ext = fileType(chunk).ext;
+                });
+
+                imgRes.on('data', chunk => {
+                    imagedata = [...imagedata, chunk];
+                })
+
+                imgRes.on('end', () => {
+
+                    const imgName = `public/images/${uuidV4()}.${ext}`;
+
+                    const buffer = Buffer.concat(imagedata);
+
+                    fs.writeFile(imgName, buffer, err => {
+
+                        if (err) throw err;
+
+                        im.resize({
+                            srcPath: imgName,
+                            dstPath: imgName,
+                            width: 50
+                        }, (err, stdout, stderr) => {
+                            if (err) throw err;
+                            console.log('image resized');
+                        });
+
+                    }); // end if write file
+
+                }); // end of res-end
+
+            })
+            .on('error', error => {
+                console.log('we have got problem: ', error);
+            });
+
+
+
+            // http.get(url, res => {
+            //     res.once('data', chunk => {
+            //
+            //         res.destroy();
+            //
+            //         const { ext } = fileType(chunk);
+            //
+            //         im.resize({
+            //             srcPath: url,
+            //             dstPath: `public/images/${uuidV4()}.${ext}`,
+            //             width: 50
+            //         }, (err, stdout, stderr) => {
+            //             if (err) throw err;
+            //             console.log('image resized');
+            //         });
+            //
+            //     });
+            // });
+
 
             res.status(HTTP.OK).json({
                 url
