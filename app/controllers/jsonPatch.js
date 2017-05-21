49272e017afc5fd77c85e2e233637befc61da3dc
Raghav Garg <ansraghav100@gmail.com>
2017-05-21 18:07:50 +0530

    added jsonpatch

--- app/controllers/jsonPatch.js
+++ app/controllers/jsonPatch.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import JsonPatch from 'json-patch';
+
 import HTTP from "../helpers/httpcodes";
 import { isExist } from "../helpers/methods";
 
@@ -11,9 +13,18 @@ module.exports = (router, middlewares) => {
 
         .post( (req, res, next) => {
 
-            res.status(HTTP.OK).json({
-                message: "hello world"
-            });
+            const jsonBody = req.body.json;
+            const jsonPatch = req.body.patch;
+
+            if (!isExist(jsonBody) || !isExist(jsonPatch)) {
+                return res.status(HTTP.BAD_REQUEST).json({
+                    message: "Both json and patch are required fields"
+                });
+            }
+
+            const result = JsonPatch.apply(jsonBody, jsonPatch);
+
+            res.status(HTTP.OK).json(result);
 
         } );
 
