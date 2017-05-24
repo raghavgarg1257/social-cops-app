77d81e850876488db97dc2519a502c57e1292976
Raghav Garg <ansraghav100@gmail.com>
2017-05-24 21:04:55 +0530

    added test suite using mocha

--- test/index.js
+++ test/index.js
@@ -2,8 +2,30 @@ import request from 'request';
 import assert from 'assert';
 
 import '../index.js';
+import { isExist } from '../app/helpers/methods';
 
 const url = "http://localhost:8000";
+
+function getAuthToken() {
+    const testUrl = url + '/login';
+    return new Promise( (resolve, reject) => {
+        request.post(testUrl, {
+            form: {
+                'username':'anystring',
+                'password':'anystring'
+            }
+        }, (err, res) => {
+            const body = JSON.parse(res.body);
+            if (isExist(body.data.token)) {
+                resolve(body.data.token);
+            }
+            else {
+                reject(err);
+            }
+        });
+    });
+}
+
 
 describe('Node Server', () => {
 
@@ -29,26 +51,88 @@ describe('Node Server', () => {
 
 
 describe('Login', function() {
+
     describe('getJwtToken()', function() {
-
-        const testUrl = url + '/login';
-
-        it("returns status 200", () => {
+        it('should return a jwt token', done => {
+            const testUrl = url + '/login';
             request.post(testUrl, {
-                username:'vfdv', password:'cd'
+                form: {
+                    'username':'anystring',
+                    'password':'anystring'
+                }
             }, (err, res) => {
+                const body = JSON.parse(res.body);
                 assert.equal(200, res.statusCode);
+                assert.notEqual(null, body.data.token);
                 done();
             });
         });
-
-        it('should return a jwt token when provided username and password', function() {
-            request.post(testUrl, (err, res) => {
-                assert.equal(200, res.statusCode);
-                done();
-            });
-            assert.equal(-1, [1,2,3].indexOf(4));
-        });
-
     });
+
+});
+
+
+describe('JSON Patch', function() {
+
+    describe('applyJsonPatch()', function() {
+        it('should return a JSON Object', done => {
+
+            const testUrl = url + '/json-patch';
+
+            getAuthToken() // for protected route
+            .then( token => {
+                request.post(testUrl, {
+                    headers: {
+                        'Authorization' : `Bearer ${token}`
+                    },
+                    form: {
+                        'json': { "baz": "qux", "foo": "bar" },
+                        'patch': [
+                            { "op": "replace", "path": "/baz", "value": "boo" },
+                            { "op": "add", "path": "/hello", "value": ["world"] },
+                            { "op": "remove", "path": "/foo"}
+                        ]
+                    }
+                }, (err, res) => {
+                    const body = JSON.parse(res.body);
+                    assert.equal(200, res.statusCode);
+                    assert.equal(undefined, body.error);
+                    done();
+                });
+            })
+            .catch();
+
+        });
+    });
+
+});
+
+
+describe('Image Thumbnail', function() {
+
+    describe('getImageThumbnail()', function() {
+        it('should return a JSON Object', done => {
+
+            const testUrl = url + '/img-thumb';
+
+            getAuthToken() // for protected route
+            .then( token => {
+                request.post(testUrl, {
+                    headers: {
+                        'Authorization' : `Bearer ${token}`
+                    },
+                    form: {
+                        'url' : 'http://res.cloudinary.com/demo/basketball_shot.jpg'
+                    }
+                }, (err, res) => {
+                    assert.equal(200, res.statusCode);
+                    assert.equal(undefined, res.body.error);
+                    done();
+                });
+            })
+            .catch();
+
+        }).timeout(10000);
+    });
+
 });
